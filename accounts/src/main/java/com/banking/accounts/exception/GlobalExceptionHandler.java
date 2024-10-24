787bf49cd470252fb44f4c9defe1a7f340403a27
mukul <mukulsharma181@gmail.com>
2024-10-24 23:27:48 +0530

    added global exception handler

--- accounts/src/main/java/com/banking/accounts/exception/GlobalExceptionHandler.java
+++ accounts/src/main/java/com/banking/accounts/exception/GlobalExceptionHandler.java
@@ -12,6 +12,15 @@ import java.time.LocalDateTime;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponseDto> handleGlobalException(CustomerAlreadyExistsException customerAlreadyExistsException,
+                                                                                 WebRequest webRequest) {
+        ErrorResponseDto errorResponseDto = new ErrorResponseDto(webRequest.getDescription(false),
+                HttpStatus.INTERNAL_SERVER_ERROR, customerAlreadyExistsException.getMessage(), LocalDateTime.now());
+
+        return new ResponseEntity<>(errorResponseDto, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
     @ExceptionHandler(CustomerAlreadyExistsException.class)
     public ResponseEntity<ErrorResponseDto> handleCustomerAlreadyExistsException(CustomerAlreadyExistsException customerAlreadyExistsException,
                                                                                  WebRequest webRequest) {
